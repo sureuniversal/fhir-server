87e82fbf4bdb322f9c188b68fd5e962f2a22121b
ehudofer <ehudo42@gmail.com>
2020-07-30 18:08:03 +0300

    Token Validation Interceptor

--- src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
@@ -8,19 +8,39 @@ import com.mongodb.client.MongoDatabase;
 
 
 import org.bson.Document;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.print.Doc;
 
 import static com.mongodb.client.model.Filters.eq;
 
 public class Utils {
-  //retunrs a user document
-  public static org.bson.Document AuthenticateToken(String authToken) {
+  //returns a user document
+  public static class TokenNotFoundException extends Exception{
+    public TokenNotFoundException(){
+      super("Token not found");
+    }
+  }
+  public static class TokenExpiredException extends Exception{
+    public TokenExpiredException(){
+      super("Token has expired");
+    }
+  }
+  public static org.bson.Document AuthenticateToken(String authToken) throws TokenNotFoundException,TokenExpiredException{
     String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
     MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString));
     MongoDatabase database = mongoClient.getDatabase("users");
     MongoCollection<org.bson.Document> oAuthAccessTokensCollection = database.getCollection("OAuthAccessToken");
     org.bson.Document authTokenDocument = oAuthAccessTokensCollection.find(eq("accessToken", authToken)).first();
+    if(authTokenDocument==null){
+      throw new TokenNotFoundException();
+    }
+    long expireTime = authTokenDocument.getDate("issuedAt").getTime()/1000+ authTokenDocument.getInteger("expiresIn");
+    long now = java.time.Instant.now().getEpochSecond();
+    if (expireTime < now)
+    {
+      throw new TokenExpiredException();
+    }
     return authTokenDocument;
   }
 
@@ -33,11 +53,17 @@ public class Utils {
   }
 
   public static org.bson.Document GetUserByToken(String authToken) {
-        Document authTokenDocument = AuthenticateToken(authToken);
-        if(authTokenDocument != null){
-          return GetUserByID(authTokenDocument.getString("uid"));
-        }
-        return null;
+    Document authTokenDocument;
+    try {
+      authTokenDocument = AuthenticateToken(authToken);
+    }
+    catch (Exception e) {
+      return null;
+    }
+    if (authTokenDocument != null) {
+      return GetUserByID(authTokenDocument.getString("uid"));
+    }
+    return null;
   }
 
   public static org.bson.Document GetClientByToken(String verificationToken) {
