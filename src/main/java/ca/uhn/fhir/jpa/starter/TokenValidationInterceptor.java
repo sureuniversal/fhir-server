b89e4ae56ecb5d2967db4f289a29cce16fc6374f
ehudofer <ehudo42@gmail.com>
2020-08-04 18:02:19 +0300

    TokenValidationInterceptor code tidying

--- src/main/java/ca/uhn/fhir/jpa/starter/TokenValidationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/TokenValidationInterceptor.java
@@ -20,16 +20,6 @@ public class TokenValidationInterceptor extends AuthorizationInterceptor {
   @Override
   public List<IAuthRule> buildRuleList(RequestDetails theRequestDetails) {
 
-    // Process authorization header - The following is a fake
-    // implementation. Obviously we'd want something more real
-    // for a production scenario.
-    //
-    // In this basic example we have two hardcoded bearer tokens,
-    // one which is for a user that has access to one patient, and
-    // another that has full access.
-    IdType userIdPatientId = null;
-    boolean userIsAdmin = false;
-    Document tokenDoc = null;
     String authHeader = theRequestDetails.getHeader("Authorization");
     if (authHeader == null){
       throw new AuthenticationException("Missing or invalid Authorization header value");
@@ -40,38 +30,14 @@ public class TokenValidationInterceptor extends AuthorizationInterceptor {
     } catch (Exception e){
       throw new AuthenticationException("Missing or invalid Authorization header value");
     }
-    try {
-      tokenDoc = Utils.AuthenticateToken(token);
-    } catch (Utils.TokenNotFoundException e) {
-      throw new AuthenticationException("Missing or invalid Authorization header value");
-    } catch (Utils.TokenExpiredException ignored) {
-      //throw new AuthenticationException("Token has expired");
-    }
-    userIsAdmin=true;
-
-    // If the user is a specific patient, we create the following rule chain:
-    // Allow the user to read anything in their own patient compartment
-    // Allow the user to write anything in their own patient compartment
-    // If a client request doesn't pass either of the above, deny it
-    if (userIdPatientId != null) {
-      return new RuleBuilder()
-        .allow().read().allResources().inCompartment("Patient", userIdPatientId).andThen()
-        .allow().write().allResources().inCompartment("Patient", userIdPatientId).andThen()
-        .denyAll()
-        .build();
-    }
 
     // If the user is an admin, allow everything
-    if (userIsAdmin) {
+    if (Utils.isTokenValid(token)) {
       return new RuleBuilder()
         .allowAll()
         .build();
+    }else {
+      throw new AuthenticationException("Missing or invalid Authorization header value");
     }
-
-    // By default, deny everything. This should never get hit, but it's
-    // good to be defensive
-    return new RuleBuilder()
-      .denyAll()
-      .build();
   }
 }
