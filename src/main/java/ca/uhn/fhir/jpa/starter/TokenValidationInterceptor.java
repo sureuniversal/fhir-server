f34fdf7bdecbdd2ad1ef5ff6d93144ba902f86db
ehudofer <ehudo42@gmail.com>
2020-08-04 21:37:35 +0300

    TokenValidationInterceptor code tidying 2

--- src/main/java/ca/uhn/fhir/jpa/starter/TokenValidationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/TokenValidationInterceptor.java
@@ -3,12 +3,9 @@ package ca.uhn.fhir.jpa.starter;
 import ca.uhn.fhir.interceptor.api.Interceptor;
 import ca.uhn.fhir.jpa.starter.oauth.Utils;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
-import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
 import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
 import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
-import org.bson.Document;
-import org.hl7.fhir.r4.model.IdType;
 
 import java.util.List;
 
@@ -22,22 +19,20 @@ public class TokenValidationInterceptor extends AuthorizationInterceptor {
 
     String authHeader = theRequestDetails.getHeader("Authorization");
     if (authHeader == null){
-      throw new AuthenticationException("Missing or invalid Authorization header value");
+      return new RuleBuilder()
+        .denyAll()
+        .build();
     }
-    String token;
-    try {
-      token = authHeader.split(" ")[1];
-    } catch (Exception e){
-      throw new AuthenticationException("Missing or invalid Authorization header value");
-    }
+    String token = authHeader.replace("Bearer ","");
 
-    // If the user is an admin, allow everything
     if (Utils.isTokenValid(token)) {
       return new RuleBuilder()
         .allowAll()
         .build();
     }else {
-      throw new AuthenticationException("Missing or invalid Authorization header value");
+      return new RuleBuilder()
+        .denyAll()
+        .build();
     }
   }
 }
