86cc906291e295c4661fcb56e1ff9a9f9e5fee0f
ehudofer <ehudo42@gmail.com>
2020-08-04 21:37:35 +0300

    TokenValidationInterceptor code tidying 2

--- src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
@@ -14,7 +14,6 @@ import javax.print.Doc;
 import static com.mongodb.client.model.Filters.eq;
 
 public class Utils {
-  // use this
   private static MongoDatabase usersDB = null;
   static
   {
@@ -22,20 +21,9 @@ public class Utils {
      MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString));
      usersDB = mongoClient.getDatabase("users");
   }
- 
-  // remove this method
-  private static void init()
-  {
-    String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
-    MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString));
-    usersDB = mongoClient.getDatabase("users");
-  }
-  
+
   public static org.bson.Document AuthenticateToken(String authToken) {
-    // remove this statement
-    if(usersDB == null){
-      init();
-    }
+
     MongoCollection<org.bson.Document> oAuthAccessTokensCollection = usersDB.getCollection("OAuthAccessToken");
     org.bson.Document authTokenDocument = oAuthAccessTokensCollection.find(eq("accessToken", authToken)).first();
     return authTokenDocument;
