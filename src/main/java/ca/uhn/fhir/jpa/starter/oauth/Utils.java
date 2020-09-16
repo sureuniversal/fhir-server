0876bfc74f4198513e31a5a9c8eec0db99c0e052
fadiCoapp <regev01@walla.co.il>
2020-09-16 17:37:23 +0300

    authorization

--- src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
@@ -1,20 +1,14 @@
 package ca.uhn.fhir.jpa.starter.oauth;
 
-import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-import org.bson.Document;
-import org.springframework.web.client.HttpClientErrorException;
-
-import javax.print.Doc;
-
 import static com.mongodb.client.model.Filters.eq;
 
 public class Utils {
-  private static MongoDatabase usersDB = null;
+  private static MongoDatabase usersDB;
   static
   {
      String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
@@ -22,14 +16,14 @@ public class Utils {
      usersDB = mongoClient.getDatabase("users");
   }
 
-  public static org.bson.Document AuthenticateToken(String authToken) {
-
+  public static String getUserIdIdExists(String authToken) {
     MongoCollection<org.bson.Document> oAuthAccessTokensCollection = usersDB.getCollection("OAuthAccessToken");
-    org.bson.Document authTokenDocument = oAuthAccessTokensCollection.find(eq("accessToken", authToken)).first();
-    return authTokenDocument;
-  }
-  
-  public static boolean isTokenValid(String authToken){
-    return AuthenticateToken(authToken)!=null;
+    var authTokenDocument = oAuthAccessTokensCollection.find(eq("accessToken", authToken)).first();
+    if (authTokenDocument != null)
+    {
+      return authTokenDocument.getString("uid");
+    }
+
+    return null;
   }
 }
