58cd29a8fd16d175e3feab4d52a57d5262b1f42d
ehudofer <ehudo42@gmail.com>
2020-09-21 16:22:53 +0300

    practitioner auth rules

--- src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
@@ -1,6 +1,5 @@
 package ca.uhn.fhir.jpa.starter.oauth;
 
-import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -15,7 +14,7 @@ import javax.print.Doc;
 import static com.mongodb.client.model.Filters.eq;
 
 public class Utils {
-  private static MongoDatabase usersDB = null;
+  private static MongoDatabase usersDB;
   static
   {
      String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
@@ -31,21 +30,15 @@ public class Utils {
   }
 
   public static org.bson.Document GetUserByID(String userID) {
-    String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
-    MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString));
-    MongoDatabase database = mongoClient.getDatabase("users");
-    MongoCollection<org.bson.Document> usersCollection = database.getCollection("user");
+
+    MongoCollection<org.bson.Document> usersCollection = usersDB.getCollection("user");
     return usersCollection.find(eq("_id", userID)).first();
   }
 
   public static org.bson.Document GetUserByToken(String authToken) {
     Document authTokenDocument;
-    try {
-      authTokenDocument = AuthenticateToken(authToken);
-    }
-    catch (Exception e) {
-      return null;
-    }
+    authTokenDocument = AuthenticateToken(authToken);
+
     if (authTokenDocument != null) {
       return GetUserByID(authTokenDocument.getString("uid"));
     }
@@ -53,11 +46,8 @@ public class Utils {
   }
 
   public static org.bson.Document GetClientByToken(String verificationToken) {
-
-    String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
-    MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString));
-    MongoDatabase database = mongoClient.getDatabase("users");
-    MongoCollection<org.bson.Document> oAuthClientApplication = database.getCollection("OAuthClientApplication");
+;
+    MongoCollection<org.bson.Document> oAuthClientApplication = usersDB.getCollection("OAuthClientApplication");
     org.bson.Document client  =  oAuthClientApplication.find(eq("verificationToken",verificationToken)).first();
     if(client != null){
       return client;
@@ -73,5 +63,9 @@ public class Utils {
 
   public static boolean isTokenValid(String authToken){
     return AuthenticateToken(authToken)!=null;
+  }
+  public static boolean isPractitioner(String authToken){
+    Document client = GetUserByToken(authToken);
+    return client.getBoolean("isPractitioner");
   }
 }
