40ae08ab650228c18ba4d794a62a52e16842de0c
ehudofer <ehudo42@gmail.com>
2020-10-05 17:45:09 +0300

    Device & DeviceMetric

--- src/main/java/ca/uhn/fhir/jpa/starter/TokenValidationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/TokenValidationInterceptor.java
@@ -9,14 +9,14 @@ import ca.uhn.fhir.rest.gclient.ReferenceClientParam;
 import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
 import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.bson.Document;
 import org.hl7.fhir.instance.model.api.IIdType;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.IdType;
-import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Interceptor
 public class TokenValidationInterceptor extends AuthorizationInterceptor {
@@ -49,24 +49,17 @@ public class TokenValidationInterceptor extends AuthorizationInterceptor {
 
         FhirContext ctx = theRequestDetails.getFhirContext();
 
+        //debug
+        HttpClient httpClient = HttpClientBuilder.create().setConnectionTimeToLive(240, TimeUnit.SECONDS).build();
+        ctx.getRestfulClientFactory().setHttpClient(httpClient);
+
+
         IGenericClient client = ctx.newRestfulGenericClient(theRequestDetails.getFhirServerBase());
 
         RuleBuilder ruleBuilder = new RuleBuilder();
-        Bundle patientBundle = (Bundle) client.search().forResource(Patient.class)
-          .where(new ReferenceClientParam("general-practitioner")
-          .hasId(userDoc.getString("_id")))
-          .withAdditionalHeader("Authorization", authHeader)
-          .execute();
 
-        for (Bundle.BundleEntryComponent item: patientBundle.getEntry()){
-          Resource resource = item.getResource();
-          IIdType userIdPatientId = new IdType("Patient", resource.getIdElement().getIdPart());
-          ruleBuilder
-            .allow().metadata().andThen()
-            .allow().patch().allRequests().andThen()
-            .allow().read().allResources().inCompartment("Patient", userIdPatientId).andThen()
-            .allow().write().allResources().inCompartment("Patient", userIdPatientId);
-        }
+        patientRules(ruleBuilder,client,userDoc.getString("_id"),authHeader);
+
         IIdType userIdPractitionerId = new IdType("Practitioner",userDoc.getString("_id"));
         ruleBuilder
           .allow().metadata().andThen()
@@ -92,5 +85,47 @@ public class TokenValidationInterceptor extends AuthorizationInterceptor {
         .denyAll("invalid token")
         .build();
     }
+  }
+  private static RuleBuilder patientRules(RuleBuilder ruleBuilder,IGenericClient client,String practitioner,String authHeader){
+    Bundle patientBundle = (Bundle) client.search().forResource(Patient.class)
+      .where(new ReferenceClientParam("general-practitioner")
+        .hasId(practitioner))
+      .withAdditionalHeader("Authorization", authHeader)
+      .execute();
+
+    for (Bundle.BundleEntryComponent item: patientBundle.getEntry()){
+      Resource resource = item.getResource();
+      IIdType userIdPatientId = new IdType("Patient", resource.getIdElement().getIdPart());
+
+      Bundle deviceBundle = (Bundle)client.search().forResource(Device.class)
+        .where(new ReferenceClientParam("patient").hasId(resource.getIdElement().getIdPart()))
+        .withAdditionalHeader("Authorization", authHeader)
+        .prettyPrint()
+        .execute();
+      for (Bundle.BundleEntryComponent item2: deviceBundle.getEntry()){
+        Resource resource2 = item2.getResource();
+        IIdType userIdDeviceId = new IdType("Device", resource2.getIdElement().getIdPart());
+
+        Bundle deviceMetricBundle = (Bundle) client.search().forResource(DeviceMetric.class)
+          .where(new ReferenceClientParam("source").hasId(userIdDeviceId))
+          .prettyPrint()
+          .execute();
+        for (Bundle.BundleEntryComponent item3: deviceMetricBundle.getEntry()){
+          Resource resource3 = item3.getResource();
+          IIdType userIdDeviceMetricId = new IdType("DeviceMetric", resource3.getIdElement().getIdPart());
+          ruleBuilder
+            .allow().read().resourcesOfType("DeviceMetric").inCompartment("DeviceMetric", userIdDeviceMetricId).andThen()
+            .allow().write().resourcesOfType("DeviceMetric").inCompartment("DeviceMetric", userIdDeviceMetricId);
+        }
+
+        ruleBuilder
+          .allow().read().resourcesOfType("Device").inCompartment("Device", userIdDeviceId).andThen()
+          .allow().write().resourcesOfType("Device").inCompartment("Device", userIdDeviceId);
+      }
+      ruleBuilder
+        .allow().read().allResources().inCompartment("Patient", userIdPatientId).andThen()
+        .allow().write().allResources().inCompartment("Patient", userIdPatientId);
+    }
+    return ruleBuilder;
   }
 }
