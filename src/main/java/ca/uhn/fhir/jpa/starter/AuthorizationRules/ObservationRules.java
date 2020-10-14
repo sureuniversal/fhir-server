ffea80af879a88c0628061b0e3878b160d067eb2
fadiCoapp <regev01@walla.co.il>
2020-10-14 12:34:33 +0300

    code review

---
+++ src/main/java/ca/uhn/fhir/jpa/starter/AuthorizationRules/ObservationRules.java
@@ -0,0 +1,37 @@
+package ca.uhn.fhir.jpa.starter.AuthorizationRules;
+
+import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
+import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObservationRules extends RuleBase {
+  {
+    this.denyMessage = "cant access Observation";
+  }
+
+  @Override
+  public List<IAuthRule> HandleGet() {
+    var ruleList = new ArrayList<IAuthRule>();
+    var patientRule = new RuleBuilder().allow().read().resourcesOfType("Observation").inCompartment("Patient", this.userIds).build();
+    var denyRule = this.DenyRule();
+    ruleList.addAll(patientRule);
+    ruleList.addAll(denyRule);
+
+    return ruleList;
+  }
+
+  @Override
+  public List<IAuthRule> HandlePost() {
+    var ruleList = new ArrayList<IAuthRule>();
+    var patientRule = new RuleBuilder().allow().write().resourcesOfType("Observation").inCompartment("Patient", this.userIds).build();
+    var patchRule = this.PatchRule();
+    var denyRule = this.DenyRule();
+    ruleList.addAll(patientRule);
+    ruleList.addAll(patchRule);
+    ruleList.addAll(denyRule);
+
+    return ruleList;
+  }
+}
