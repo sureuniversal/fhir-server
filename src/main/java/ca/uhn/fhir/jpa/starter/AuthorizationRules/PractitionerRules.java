c8a5464ae97c5946473a3ca0c53150dd0fa2ac36
ehudofer <ehudo42@gmail.com>
2020-10-15 17:02:27 +0300

    works as needed

--- src/main/java/ca/uhn/fhir/jpa/starter/AuthorizationRules/PractitionerRules.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/AuthorizationRules/PractitionerRules.java
@@ -6,29 +6,24 @@ import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PatientRule extends RuleBase {
-  {
-    this.denyMessage = "Patient can only access himself";
-  }
-
+public class PractitionerRules extends RuleBase {
   @Override
   public List<IAuthRule> HandleGet() {
     List<IAuthRule> ruleList = new ArrayList<>();
-    List<IAuthRule> patientRule = new RuleBuilder().allow().read().allResources().inCompartment("Patient", this.userIds).build();
+    List<IAuthRule> practitionerRule = new RuleBuilder().allow().read().allResources().inCompartment("Practitioner", this.practitionerId).build();
     List<IAuthRule> denyRule = DenyRule();
-    ruleList.addAll(patientRule);
+    ruleList.addAll(practitionerRule);
     ruleList.addAll(denyRule);
 
     return ruleList;
   }
 
   @Override
-  public List<IAuthRule> HandlePost() {
-    List<IAuthRule> ruleList = new ArrayList<>();
-    List<IAuthRule> patientRule = new RuleBuilder().allow().write().allResources().inCompartment("Patient", this.userIds).build();
+  public List<IAuthRule> HandlePost() {    List<IAuthRule> ruleList = new ArrayList<>();
+    List<IAuthRule> practitionerRule = new RuleBuilder().allow().write().allResources().inCompartment("Practitioner", this.practitionerId).build();
     List<IAuthRule> patchRule = PatchRule();
     List<IAuthRule> denyRule = DenyRule();
-    ruleList.addAll(patientRule);
+    ruleList.addAll(practitionerRule);
     ruleList.addAll(patchRule);
     ruleList.addAll(denyRule);
 
