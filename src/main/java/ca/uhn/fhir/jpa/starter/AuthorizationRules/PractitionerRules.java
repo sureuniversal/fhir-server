3a322c21fd64570feca84369e2fbf2d1567bf244
ehudofer <ehudo42@gmail.com>
2020-10-15 17:38:31 +0300

    clean up & small fix

--- src/main/java/ca/uhn/fhir/jpa/starter/AuthorizationRules/PractitionerRules.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/AuthorizationRules/PractitionerRules.java
@@ -7,23 +7,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PractitionerRules extends RuleBase {
+
+  {
+    denyMessage = "Not a Practitioner or not authorized";
+  }
+
   @Override
   public List<IAuthRule> HandleGet() {
     List<IAuthRule> ruleList = new ArrayList<>();
-    List<IAuthRule> practitionerRule = new RuleBuilder().allow().read().allResources().inCompartment("Practitioner", this.practitionerId).build();
     List<IAuthRule> denyRule = DenyRule();
-    ruleList.addAll(practitionerRule);
+    if(practitionerId != null){
+      List<IAuthRule> practitionerRule = new RuleBuilder().allow().read().allResources().inCompartment("Practitioner", practitionerId).build();
+      ruleList.addAll(practitionerRule);
+    }
     ruleList.addAll(denyRule);
 
     return ruleList;
   }
 
   @Override
-  public List<IAuthRule> HandlePost() {    List<IAuthRule> ruleList = new ArrayList<>();
-    List<IAuthRule> practitionerRule = new RuleBuilder().allow().write().allResources().inCompartment("Practitioner", this.practitionerId).build();
+  public List<IAuthRule> HandlePost() {
+    List<IAuthRule> ruleList = new ArrayList<>();
     List<IAuthRule> patchRule = PatchRule();
     List<IAuthRule> denyRule = DenyRule();
-    ruleList.addAll(practitionerRule);
+    if(practitionerId != null){
+      List<IAuthRule> practitionerRule = new RuleBuilder().allow().write().allResources().inCompartment("Practitioner", practitionerId).build();
+      ruleList.addAll(practitionerRule);
+    }
     ruleList.addAll(patchRule);
     ruleList.addAll(denyRule);
 
