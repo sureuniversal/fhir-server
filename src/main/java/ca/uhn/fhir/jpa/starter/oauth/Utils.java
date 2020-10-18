60076ac76a65ec998226a7c66e06d76735da3b98
ehudofer <ehudo42@gmail.com>
2020-10-18 16:15:59 +0300

    TokenRecord postgre

--- src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/oauth/Utils.java
@@ -6,15 +6,33 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.sql.*;
+
 import static com.mongodb.client.model.Filters.eq;
 
 public class Utils {
-  private static MongoDatabase usersDB;
+  private static MongoDatabase usersDB = null;
+//  private static Connection postgreCon = null;
+  private static Statement postgreStm = null;
+  private static final boolean isPostgre;
+
   static
   {
-     String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
-     MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString));
-     usersDB = mongoClient.getDatabase("users");
+    isPostgre = System.getenv("FHIR_PG_TOKEN_URL") == null;
+    if(isPostgre) {
+      String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
+      MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString));
+      usersDB = mongoClient.getDatabase("users");
+    } else {
+      try {
+        String connectionString =System.getenv("FHIR_PG_TOKEN_URL");
+        Connection postgreCon = DriverManager.getConnection(connectionString);
+        postgreCon.setAutoCommit(false);
+        postgreStm = postgreCon.createStatement();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
   }
 
   public static org.bson.Document AuthenticateToken(String authToken) {
@@ -41,7 +59,33 @@ public class Utils {
   }
 
   public static TokenRecord getTokenRecord(String token){
+    if(isPostgre){
+      return getTokenRecordPostgre(token);
+    }
     return getTokenRecordMongo(token);
+  }
+
+  public static TokenRecord getTokenRecordPostgre(String token){
+    try {
+      ResultSet resultSet = postgreStm.executeQuery("select u.\"id\", u.ispractitioner, o.accesstoken, o.issuedat, o.expiresin"+
+        "from \"public\".oauthaccesstoken o,\"public\".user u "+
+        "where o.uid = u.\"id\" and o.accesstoken = '"+token+"';");
+      String userId = resultSet.getString("id");
+      boolean isPractitioner = resultSet.getBoolean("ispractitioner");
+      long issued = -1;
+      long expire = -1;
+      try {
+        issued = resultSet.getDate("issuedat").getTime()/1000;
+        expire = resultSet.getLong("expiresin");
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+      return new TokenRecord(userId,token,isPractitioner,issued,expire);
+    } catch (SQLException e) {
+      org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(Utils.class);
+      ourLog.error("postgreSQL error:",e);
+      return null;
+    }
   }
 
   public static TokenRecord getTokenRecordMongo(String token){
