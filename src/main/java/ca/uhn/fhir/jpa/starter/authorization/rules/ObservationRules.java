b0a4192ef0521dccf610f38a8768d5549d6f0ca8
ehudofer <ehudo42@gmail.com>
2020-10-20 13:12:56 +0300

    Refactor

--- src/main/java/ca/uhn/fhir/jpa/starter/authorization/rules/ObservationRules.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/authorization/rules/ObservationRules.java
@@ -1,4 +1,4 @@
-package ca.uhn.fhir.jpa.starter.AuthorizationRules;
+package ca.uhn.fhir.jpa.starter.authorization.rules;
 
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
 import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
@@ -7,29 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ObservationRules extends RuleBase {
-  {
+  public ObservationRules() {
     this.denyMessage = "cant access Observation";
   }
 
   @Override
-  public List<IAuthRule> HandleGet() {
+  public List<IAuthRule> handleGet() {
     List<IAuthRule> ruleList = new ArrayList<>();
     List<IAuthRule> patientRule = new RuleBuilder().allow().read().allResources().inCompartment("Patient", this.userIds).build();
-    List<IAuthRule> denyRule = DenyRule();
+    List<IAuthRule> commonRules = commonRules();
+    List<IAuthRule> denyRule = denyRule();
     ruleList.addAll(patientRule);
+    ruleList.addAll(commonRules);
     ruleList.addAll(denyRule);
 
     return ruleList;
   }
 
   @Override
-  public List<IAuthRule> HandlePost() {
+  public List<IAuthRule> handlePost() {
     List<IAuthRule> ruleList = new ArrayList<>();
     List<IAuthRule> patientRule = new RuleBuilder().allow().write().allResources().inCompartment("Patient", this.userIds).build();
-    List<IAuthRule> patchRule = PatchRule();
-    List<IAuthRule> denyRule = DenyRule();
+    List<IAuthRule> commonRules = commonRules();
+    List<IAuthRule> denyRule = denyRule();
     ruleList.addAll(patientRule);
-    ruleList.addAll(patchRule);
+    ruleList.addAll(commonRules);
     ruleList.addAll(denyRule);
 
     return ruleList;
