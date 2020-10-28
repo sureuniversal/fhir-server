406eda1f3c2430716066276787a11a09a9a95b82
ehudofer <ehudo42@gmail.com>
2020-10-28 15:03:36 +0200

    add create rule

--- src/main/java/ca/uhn/fhir/jpa/starter/authorization/rules/PractitionerRules.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/authorization/rules/PractitionerRules.java
@@ -1,7 +1,10 @@
 package ca.uhn.fhir.jpa.starter.authorization.rules;
 
+import org.hl7.fhir.r4.model.Practitioner;
+
 public class PractitionerRules extends GeneralRules {
   public PractitionerRules(String authHeader) {
     super(authHeader);
+    type = Practitioner.class;
   }
 }
