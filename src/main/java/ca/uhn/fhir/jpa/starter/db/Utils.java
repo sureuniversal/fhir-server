7d1b301bc97083baf411d354329891e9bd370abd
ehud <ehudo42@gmail.com>
2020-12-28 12:40:59 +0200

    delete mongo

--- src/main/java/ca/uhn/fhir/jpa/starter/db/Utils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/db/Utils.java
@@ -1,7 +1,6 @@
 package ca.uhn.fhir.jpa.starter.db;
 
 import ca.uhn.fhir.jpa.starter.authorization.rules.*;
-import ca.uhn.fhir.jpa.starter.db.interactor.DBInteractorMongo;
 import ca.uhn.fhir.jpa.starter.db.interactor.DBInteractorPostgres;
 import ca.uhn.fhir.jpa.starter.db.interactor.IDBInteractor;
 import ca.uhn.fhir.jpa.starter.db.token.TokenRecord;
@@ -19,15 +18,10 @@ public class Utils {
   private final static Map<String,TokenRecord> tokenCash = new HashMap<>();
 
   static {
-    if (System.getenv("FHIR_PG_TOKEN_URL") == null) {
-      String connectionString = System.getenv("FHIR_MONGO_DATASOURCE_URL");
-      interactor = new DBInteractorMongo(connectionString);
-    } else {
       String connectionString = System.getenv("FHIR_PG_TOKEN_URL");
       String postgresUser = System.getenv("FHIR_PG_TOKEN_USER_NAME");
       String postgresPass = System.getenv("FHIR_PG_TOKEN_PASSWORD");
       interactor = new DBInteractorPostgres(connectionString, postgresUser, postgresPass);
-    }
   }
   
   public static TokenRecord getTokenRecord(String token) {
