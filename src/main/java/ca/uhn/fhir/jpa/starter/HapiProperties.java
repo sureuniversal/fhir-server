702c24495da8b932fcf54d25c46723a1f9b75a7d
ehud <ehudo42@gmail.com>
2021-01-03 12:47:24 +0200

    incoming log format

--- src/main/java/ca/uhn/fhir/jpa/starter/HapiProperties.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/HapiProperties.java
@@ -47,6 +47,7 @@ public class HapiProperties {
   static final String HAPI_PROPERTIES = "hapi.properties";
   static final String LOGGER_ERROR_FORMAT = "logger.error_format";
   static final String LOGGER_FORMAT = "logger.format";
+  static final String LOGGER_INCOMING_FORMAT = "logger.incoming.format";
   static final String LOGGER_LOG_EXCEPTIONS = "logger.log_exceptions";
   static final String LOGGER_NAME = "logger.name";
   static final String MAX_FETCH_SIZE = "max_fetch_size";
@@ -281,6 +282,14 @@ public class HapiProperties {
     }
 
     return HapiProperties.getProperty(LOGGER_FORMAT, "Path[${servletPath}] Source[${requestHeader.x-forwarded-for}] Operation[${operationType} ${operationName} ${idOrResourceName}] UA[${requestHeader.user-agent}] Params[${requestParameters}] ResponseEncoding[${responseEncodingNoDefault}]");
+  }
+
+  public static String getLoggerIncomingFormat() {
+    if(System.getenv("LOGGER_INCOMING_FORMAT") != null){
+      return System.getenv("LOGGER_INCOMING_FORMAT");
+    }
+
+    return HapiProperties.getProperty(LOGGER_INCOMING_FORMAT, "${operationType} - ${idOrResourceName} - ${requestId}");
   }
 
   public static String getLoggerErrorFormat() {
--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -186,6 +186,7 @@ public class JpaRestfulServer extends RestfulServer {
     loggingInterceptor.setLoggerName(HapiProperties.getLoggerName());
     loggingInterceptor.setMessageFormat(HapiProperties.getLoggerFormat());
     loggingInterceptor.setErrorMessageFormat(HapiProperties.getLoggerErrorFormat());
+    loggingInterceptor.setIncomingFormat(HapiProperties.getLoggerIncomingFormat());
     loggingInterceptor.setLogExceptions(HapiProperties.getLoggerLogExceptions());
     //loggingInterceptor.setLogRequestSummary(true);
     //loggingInterceptor.setLogRequestBody(true);
