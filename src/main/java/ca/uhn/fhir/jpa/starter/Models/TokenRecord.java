b6076067cd6082c2b8f78a06c87376fea27c4713
ehud <ehudo42@gmail.com>
2021-01-31 18:09:02 +0200

    scopes

--- src/main/java/ca/uhn/fhir/jpa/starter/Models/TokenRecord.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/Models/TokenRecord.java
@@ -9,13 +9,16 @@ public class TokenRecord extends CacheRecord {
   public boolean isAdmin = false;
   final long issuedDate;
   final long expiresIn;
+  final long recordTtl;
+  final String[] scopes;
 
-  public TokenRecord(String id, String token, boolean is_practitioner, long issuedDate, long expiresIn) {
+  public TokenRecord(String id, String token, boolean is_practitioner, long issuedDate, long expiresIn, String[] scopes) {
     this.id = id;
     this.token = token;
     this.is_practitioner = is_practitioner;
     this.issuedDate = issuedDate;
     this.expiresIn = expiresIn;
+    this.scopes = scopes;
     this.recordTtl = System.currentTimeMillis() + DBUtils.getCacheTTL();
   }
 
@@ -41,5 +44,9 @@ public class TokenRecord extends CacheRecord {
 
   public boolean isRecordExpired(){
     return ((recordTtl - System.currentTimeMillis()) < 0);
+  }
+
+  public String[] getScopes() {
+    return scopes;
   }
 }
