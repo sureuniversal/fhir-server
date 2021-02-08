64b3ca120363bcbe2c9dc5ce2eab7be84054cc12
fadiCoapp <regev01@walla.co.il>
2021-02-08 18:01:19 +0200

    fix for scopes

--- src/main/java/ca/uhn/fhir/jpa/starter/interactor/DBInteractorPostgres.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/interactor/DBInteractorPostgres.java
@@ -23,7 +23,7 @@ public class DBInteractorPostgres implements IDBInteractor {
   public TokenRecord getTokenRecord(String token) {
     try {
       var postgresStm = postgresCon.prepareStatement(
-        "select u.id, u.ispractitioner, o.accesstoken, o.issuedat, o.expiresin, o.scopes " +
+        "select u.id, u.ispractitioner, o.accesstoken, o.issuedat, o.expiresin, false as scopes " +
           "from public.oauthaccesstoken o " +
           "join public.user u on o.uid = u.id " +
           "where o.accesstoken = '" + token + "';"
