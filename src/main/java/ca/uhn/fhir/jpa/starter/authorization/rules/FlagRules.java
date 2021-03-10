bdef5b91c5aa9c280264a827c7fe65c60754a175
fadiCoapp <regev01@walla.co.il>
2021-03-10 15:23:37 +0200

    added new pointers for sec rules IMPL

--- src/main/java/ca/uhn/fhir/jpa/starter/authorization/rules/FlagRules.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/authorization/rules/FlagRules.java
@@ -1,10 +1,24 @@
 package ca.uhn.fhir.jpa.starter.authorization.rules;
 
+import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
+import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
 import org.hl7.fhir.r4.model.Flag;
+
+import java.util.List;
 
 public class FlagRules extends PatientRules {
   public FlagRules() {
     this.denyMessage = "cant access Flag";
     this.type = Flag.class;
   }
+  // sec rules updates
+  // We need to check if the request body to see if the subject for the flag is referencing the sending user
+  @Override
+  public List<IAuthRule> handlePost() {
+    return new RuleBuilder().allowAll().build();
+  }
+
+
+  // sec rules updates
+  // override handleUpdate and give it a deny all
 }
--- src/main/java/ca/uhn/fhir/jpa/starter/authorization/rules/PatientRules.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/authorization/rules/PatientRules.java
@@ -17,16 +17,24 @@ public class PatientRules extends RuleBase {
     this.type = Patient.class;
   }
 
+
+  // sec rules updates
+  // We need to also check if the request has an organization parameter ex: Patient?organization=48b07dfb-1b3d-4232-b74c-5efec04ee3d7
+  // follow how <ref> idsParamValues </ref> is being built in RuleBase see <ref> setUserIdsRequested </ref>
+  // then we should allow the request if it has that parameter as well
   @Override
   public List<IAuthRule> handleGet() {
     var userIds = this.setupAllowedUsersList();
 
-    var allExits = true;
+    var existCounter = 0;
     for (var allowedId : this.idsParamValues) {
-      allExits = allExits & userIds.contains(allowedId);
+      if(userIds.contains(allowedId))
+      {
+        existCounter++;
+      }
     }
 
-    if (allExits)
+    if (existCounter == this.idsParamValues.size())
     {
       var allow = new RuleBuilder().allow().read().allResources().withAnyId();
 
@@ -45,38 +53,22 @@ public class PatientRules extends RuleBase {
     return denyRule();
   }
 
+  // sec rules updates
+  // We need to check if the request body for creating a Patient has a ManagingOrganization in it
+  // and that it equals the organization for the user sending the request same as userId for patient and
+  // see <ref> Search.getPractitionerOrganization </ref> for Practitioner
   @Override
   public List<IAuthRule> handlePost() {
     return new RuleBuilder().allowAll().build();
   }
 
+  // sec rules updates
+  // We need to check if the patient being updated has a ManagingOrganization either in the request body or by performing a search for that field in the db
+  // if it is not provided, and that it equals the organization for the user sending the request same as userId for patient and
+  // see <ref> Search.getPractitionerOrganization </ref> for Practitioner
   public List<IAuthRule> handleUpdate()
   {
     return new RuleBuilder().allowAll().build();
-//    var userIds = this.setupAllowedUsersList();
-//
-//    var allExits = true;
-//    for (var allowedId : this.idsParamValues) {
-//      allExits = allExits & userIds.contains(allowedId);
-//    }
-//
-//    if (allExits)
-//    {
-//      var allow = new RuleBuilder().allow().write().allResources().withAnyId();
-//
-//      List<IAuthRule> patientRule = allow.build();
-//      List<IAuthRule> commonRules = commonRulesPost();
-//      List<IAuthRule> denyRule = denyRule();
-//
-//      List<IAuthRule> ruleList = new ArrayList<>();
-//      ruleList.addAll(patientRule);
-//      ruleList.addAll(commonRules);
-//      ruleList.addAll(denyRule);
-//
-//      return ruleList;
-//    }
-//
-//    return denyRule();
   }
 
   private List<String> setupAllowedUsersList()
