ec68e1e4db2551f9246d483cad2262550e40a683
ehud <ehudo42@gmail.com>
2021-07-12 09:38:21 +0300

    loopback token

--- src/main/java/ca/uhn/fhir/jpa/starter/Util/DBUtils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/Util/DBUtils.java
@@ -1,6 +1,7 @@
 package ca.uhn.fhir.jpa.starter.Util;
 import ca.uhn.fhir.jpa.starter.HapiProperties;
 import ca.uhn.fhir.jpa.starter.Models.TokenRecord;
+import ca.uhn.fhir.jpa.starter.interactor.DBInteractorLoopback;
 import ca.uhn.fhir.jpa.starter.interactor.DBInteractorPostgres;
 import ca.uhn.fhir.jpa.starter.interactor.IDBInteractor;
 
@@ -9,10 +10,14 @@ public class DBUtils {
 
   static
   {
+    if(System.getenv("LOOPBACK_URL")!=null){
+      interactor = new DBInteractorLoopback(System.getenv("LOOPBACK_URL"));
+    } else {
       var connectionString = System.getenv("FHIR_PG_DATASOURCE_URL") + "/" + System.getenv("FHIR_PG_DATASOURCE_TOKEN_DB");
       var postgresUser = System.getenv("FHIR_PG_DATASOURCE_USER_NAME");
       var postgresPass = System.getenv("FHIR_PG_DATASOURCE_PASSWORD");
       interactor = new DBInteractorPostgres(connectionString, postgresUser, postgresPass);
+    }
   }
 
   public static TokenRecord getTokenRecord(String token) {
